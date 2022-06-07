c873bfd51760da6a179a6b14055baca8b6025933
wanliu <1692894189@qq.com>
2022-06-07 20:49:19 +0800

    new function

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -140,5 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  alert('Bug');
 };
 init();
